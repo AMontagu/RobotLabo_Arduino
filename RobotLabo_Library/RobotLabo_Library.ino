ed5fc199c2fd18298da2c4137f1cfeda7277f02e
AMontagu <adrienmontagu@gmail.com>
2016-05-13 20:02:53 +0200

    Ajoutez des fichiers projet.

---
+++ RobotLabo_Library/RobotLabo_Library.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		RobotLabo_Library.ino
+ Created:	13/05/2016 20:02:48
+ Author:	Adrien
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "RobotLabo_LibraryLib.h"
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ RobotLabo_Library/src/_micro-api/libraries/RobotLabo_LibraryLib/src/RobotLabo_LibraryLib.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		RobotLabo_LibraryLib.cpp
+ Created:	13/05/2016 20:02:48
+ Author:	Adrien
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "RobotLabo_LibraryLib.h"
+
+
