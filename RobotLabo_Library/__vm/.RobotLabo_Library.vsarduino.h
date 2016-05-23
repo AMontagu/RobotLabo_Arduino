a964a485988554270dd97057c304486a4f166726
AMontagu <adrienmontagu@gmail.com>
2016-05-23 15:01:19 +0200

    clean work

--- RobotLabo_Library/__vm/.RobotLabo_Library.vsarduino.h
+++ RobotLabo_Library/__vm/.RobotLabo_Library.vsarduino.h
@@ -11,7 +11,6 @@
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
 #define __AVR_ATmega2560__
-#define _VMDEBUG 1
 #define F_CPU 16000000L
 #define ARDUINO 10606
 #define ARDUINO_AVR_MEGA2560
@@ -63,17 +62,11 @@ extern "C" void __cxa_pure_virtual() {;}
 #define portOutputRegister(P)
 #define portInputRegister(P)
 #define portModeRegister(P)
+
+//
+//
+
 #include <RobotLabo_Library.ino>
-#include <ColorSensorTCS3200.cpp>
-#include <ColorSensorTCS3200.h>
-#include <Encoder.cpp>
-#include <Encoder.h>
-#include <IRSharp10To80.cpp>
-#include <IRSharp10To80.h>
 #include <src\_micro-api\libraries\RobotLabo_LibraryLib\src\RobotLabo_LibraryLib.cpp>
 #include <src\_micro-api\libraries\RobotLabo_LibraryLib\src\RobotLabo_LibraryLib.h>
-#include <Sensor.h>
-#include <UltrasonicSensorHCSR04.cpp>
-#include <UltrasonicSensorHCSR04.h>
-#include <resource.h>
 #endif
